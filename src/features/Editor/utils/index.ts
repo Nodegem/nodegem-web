a36ed588eff972b68aae6f75bd724037a3057eeb
Ryan Szemplinski <ryan.szemplinski@quintiles.com>
2018-08-14 10:23:36 -0400

    Adding GUID helper

--- src/features/Editor/utils/index.ts
+++ src/features/Editor/utils/index.ts
@@ -22,3 +22,12 @@ export const getPosition = (e: MouseEvent, node: HTMLElement) : XYCoords => {
     const offsetParent = node.offsetParent || node.ownerDocument.body;
     return offsetXYFromParent(e, offsetParent);
 }
+
+export class Guid {
+    static newGuid() {
+        return 'xxxxxxxx-xxxx-4xxx-yxxx-xxxxxxxxxxxx'.replace(/[xy]/g, function(c) {
+            var r = Math.random()*16|0, v = c == 'x' ? r : (r&0x3|0x8);
+            return v.toString(16);
+        });
+    }
+}
