72bc1b9e61a5f5c8aa9eac71e4fbd5c3228ea57d
Ryan Szemplinski <rszemplinski22@gmail.com>
2019-06-28 11:15:21 -0400

    Changing tslint to not break builds

--- src/utils/extensions.ts
+++ src/utils/extensions.ts
@@ -1,51 +1,51 @@
 import { string } from 'prop-types';
 
 declare global {
-  interface Array<T> {
-    empty(): boolean;
-    removeItem(T): Array<T>;
-    firstOrDefault(): T | null;
-    lastOrDefault(): T | null;
-    copy(): Array<T>;
-    toDictionary<T>(array: Array<T>, indexKey: keyof T) : { [key : string] : T };
-  }
+    interface Array<T> {
+        empty(): boolean;
+        removeItem(T): T[];
+        firstOrDefault(): T | null;
+        lastOrDefault(): T | null;
+        copy(): T[];
+        toDictionary<T>(array: T[], indexKey: keyof T): { [key: string]: T };
+    }
 
-  interface String {
-    upperCaseFirst(): string
-  }
+    interface String {
+        upperCaseFirst(): string;
+    }
 }
 
-Array.prototype.removeItem = function<T>(item: T): Array<T> {
-  return this.splice(this.indexOf(item), 1);
+Array.prototype.removeItem = function<T>(item: T): T[] {
+    return this.splice(this.indexOf(item), 1);
 };
 
 Array.prototype.empty = function(): boolean {
-  return this.length <= 0;
+    return this.length <= 0;
 };
 
 Array.prototype.firstOrDefault = function<T>(): T | null {
-  return !this.empty() ? this[0] : null;
+    return !this.empty() ? this[0] : null;
 };
 
 Array.prototype.lastOrDefault = function<T>(): T | null {
-  return !this.empty() ? this[this.length - 1] : null;
+    return !this.empty() ? this[this.length - 1] : null;
 };
 
-Array.prototype.copy = function<T>(): Array<T> {
-  return this.slice(0);
+Array.prototype.copy = function<T>(): T[] {
+    return this.slice(0);
 };
 
-Array.prototype.toDictionary = function <T>(array: Array<T>, indexKey: keyof T) {
-  const normalizedObject: any = {}
-  for (let i = 0; i < array.length; i++) {
-       const key = array[i][indexKey]
-       normalizedObject[key] = array[i]
-  }
-  return normalizedObject as { [key: string]: T }
+Array.prototype.toDictionary = function<T>(array: T[], indexKey: keyof T) {
+    const normalizedObject: any = {};
+    for (let i = 0; i < array.length; i++) {
+        const key = array[i][indexKey];
+        normalizedObject[key] = array[i];
+    }
+    return normalizedObject as { [key: string]: T };
 };
 
 String.prototype.upperCaseFirst = function jsUcfirst(): string {
-  return this.charAt(0).toUpperCase() + this.slice(1);
-}
+    return this.charAt(0).toUpperCase() + this.slice(1);
+};
 
 export {};
