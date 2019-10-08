ded4f643eccadab4a759f1c639d5576e8436cae3
Ryan Szemplinski <rszemplinski22@gmail.com>
2019-10-07 21:58:31 -0400

    Trying to figure out refresh token

--- src/services/agent.ts
+++ src/services/agent.ts
@@ -1,12 +1,17 @@
 import { userStore } from 'stores';
 import superagent, { SuperAgentRequest } from 'superagent';
+import { sleep } from 'utils';
 
 const ROOT_URL = process.env.REACT_APP_API_BASE_URL;
 
 export const combinePath = url => `${ROOT_URL}/api${url}`;
 
-const tokenPlugin = (req: SuperAgentRequest) => {
+const tokenPlugin = async (req: SuperAgentRequest) => {
     if (userStore && userStore.token) {
+        console.log('before');
+        await sleep(300);
+        console.log('after');
+
         req.set('Authorization', `Bearer ${userStore.token.accessToken}`);
     }
 };
@@ -18,11 +23,20 @@ const credentialPlugin = (req: SuperAgentRequest) => {
 const responseBody = (res: superagent.Response) => res.body;
 
 const handleErrors = err => {
-    if (err && err.response && err.response.status === 401) {
-        // logout
-    }
-
+    console.log(err);
     throw err;
+};
+
+const handleRetry = async (err, res: superagent.Response) => {
+    if (err && err.response && err.response.status === 401) {
+        const response = await superagent.get(
+            combinePath(`/account/refreshToken/${userStore.token!.accessToken}`)
+        );
+
+        if (response.status === 200) {
+            userStore.setToken(response.body);
+        }
+    }
 };
 
 const requests = {
@@ -31,31 +45,41 @@ const requests = {
             .get(combinePath(url))
             .use(tokenPlugin)
             .use(credentialPlugin)
-            .then(responseBody, handleErrors),
+            .retry(2, handleRetry)
+            .then(responseBody)
+            .catch(handleErrors),
     post: (url, body) =>
         superagent
             .post(combinePath(url), body)
             .use(tokenPlugin)
             .use(credentialPlugin)
-            .then(responseBody, handleErrors),
+            .retry(2, handleRetry)
+            .then(responseBody)
+            .catch(handleErrors),
     put: (url, body) =>
         superagent
             .put(combinePath(url), body)
             .use(tokenPlugin)
             .use(credentialPlugin)
-            .then(responseBody, handleErrors),
+            .retry(2, handleRetry)
+            .then(responseBody)
+            .catch(handleErrors),
     patch: (url, body) =>
         superagent
             .patch(combinePath(url), body)
             .use(tokenPlugin)
             .use(credentialPlugin)
-            .then(responseBody, handleErrors),
+            .retry(2, handleRetry)
+            .then(responseBody)
+            .catch(handleErrors),
     del: url =>
         superagent
             .del(combinePath(url))
             .use(tokenPlugin)
             .use(credentialPlugin)
-            .then(responseBody, handleErrors),
+            .retry(2, handleRetry)
+            .then(responseBody)
+            .catch(handleErrors),
 };
 
 export { requests };
